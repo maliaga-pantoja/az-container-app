08b5f65b00852f1fd50430fcb2b0621ef8fe7177
miguel <maliagap@emeal.nttdata.com>
2023-11-08 16:13:12 -0500

    adding code scan

--- devops/makefiles/security.mk
+++ devops/makefiles/security.mk
@@ -1,2 +1,4 @@
 scan_iac:
-	./devops/scripts/security.sh script_security
+	./devops/scripts/security.sh script_security_iac
+scan_app:
+	./devops/scripts/security.sh script_security_app
--- devops/scripts/security.sh
+++ devops/scripts/security.sh
@@ -5,7 +5,7 @@ script_load_env ()
     source ./devops/scripts/env.sh
 }
 
-script_security ()
+script_security_iac ()
 {
     # loading env file
     script_load_env 
@@ -13,7 +13,18 @@ script_security ()
     script_configure_env
     # running base tf script
     docker run --rm -e SNYK_TOKEN=$SNYK_TOKEN -v "$PWD/iac:/app" \
-        -w /app snyk/snyk:alpine "/usr/local/bin/snyk iac test --json --json-file-output=vuln.json"
+        -w /app snyk/snyk:alpine "/usr/local/bin/snyk iac test --json --json-file-output=iac_report.json"
+}
+
+script_security_app ()
+{
+    # loading env file
+    script_load_env 
+    # configure local env vars
+    script_configure_env
+    # running base tf script
+    docker run --rm -e SNYK_TOKEN=$SNYK_TOKEN -v "$PWD/app:/app" \
+        -w /app snyk/snyk:alpine "/usr/local/bin/snyk code test --json --json-file-output=app_report.json"
 }
 
 "$@"
