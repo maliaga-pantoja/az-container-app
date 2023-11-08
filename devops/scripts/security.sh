8efee7fc5e5f5acb51408d81088212be884be207
miguel <maliagap@emeal.nttdata.com>
2023-11-08 16:03:20 -0500

    removing -it for security script

--- devops/scripts/security.sh
+++ devops/scripts/security.sh
@@ -12,7 +12,7 @@ script_security ()
     # configure local env vars
     script_configure_env
     # running base tf script
-    docker run --rm -it -e SNYK_TOKEN=$SNYK_TOKEN -v "$PWD/iac:/app" \
+    docker run --rm -e SNYK_TOKEN=$SNYK_TOKEN -v "$PWD/iac:/app" \
         -w /app snyk/snyk:alpine "/usr/local/bin/snyk iac test --json --json-file-output=vuln.json"
 }
 
